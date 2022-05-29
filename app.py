57c0000a9630b0ef04980dbdc12f4be36541a5b7
soheeeeP <tpfj@naver.com>
2022-05-29 09:49:09 +0900

    refactor - route와 util func 분리

--- app.py
+++ app.py
@@ -1,97 +1,15 @@
 import json
-import re
 import os
 from flask import Flask, make_response
 import pandas as pd
-from sqlalchemy import create_engine
 
-from config import api_request_url, API_CLIENT_ID, API_CLIENT_SECRET, MY_SQL_DATABASE_URI, CSV_PATH, RUN_MODE
-from book_data_crawler import NaverSearch
+from config import api_request_url, API_CLIENT_ID, API_CLIENT_SECRET, CSV_PATH, RUN_MODE
+from book_data_saver import NaverSearch, prod_pubs, test_pubs, dict_to_dataframe
 
 app = Flask(__name__)
 
 
-df_column = [
-    "title",
-    "author",
-    "publisher",
-    "isbn",
-    "description",
-    "image",
-]
-
-
-prod_pubs = [
-    "위즈덤하우스",
-    "미디어그룹",
-    "시공사",
-    "문학동네",
-    "북이십일",
-    "김영사",
-    "창비",
-    "웅진싱크빅",
-    "도서출판길벗",
-    "민음사",
-    "알에이치코리아",
-    "다산북스",
-    "학지사",
-    "마더텅",
-    "아가페출판사",
-    "비룡소",
-    "한빛미디어",
-    "넥서스",
-    "박영사",
-    "쌤앤파커스",
-    "영진닷컴",
-    "가나문화콘텐츠",
-    "계림북스",
-    "을유문화사",
-    "자음과모음",
-    "개암나무"
-]
-
-test_pubs = prod_pubs[:3]
-
-
-def preprocessor(df):
-    reg_ex = '<.+?>'
-
-    df.loc[:, 'title'] = df['title'].map(lambda x: re.sub(reg_ex, '', x))
-    df.loc[:, 'author'] = df['author'].map(lambda x: re.sub(reg_ex, '', x))
-    df.loc[:, 'publisher'] = df['publisher'].map(lambda x: re.sub(reg_ex, '', x))
-    df.loc[:, 'description'] = df['description'].map(lambda x: re.sub(reg_ex, '', x))
-
-    df.loc[:, 'image'] = df['image'].map(lambda x: re.sub(reg_ex, '', x).rsplit('?', 1)[0])
-    df.loc[:, 'isbn'] = df['isbn'].map(lambda x: re.sub(reg_ex, '', x).rsplit(' ', 1)[1])
-
-    return df
-
-
-def dict_to_dataframe(items):
-    books = []
-    for item in items:
-        if item['isbn'] == '':
-            continue
-        books.append([item[col] for col in df_column])
-
-    df = pd.DataFrame(books, columns=df_column)
-    df.dropna(axis=0, how='any', inplace=True)
-
-    return preprocessor(df)
-
-
-def save_to_db(df):
-    engine = create_engine(MY_SQL_DATABASE_URI, encoding='utf-8')
-    conn = engine.connect()
-    df.to_sql('book_t', conn)
-    _df = pd.read_sql('SELECT * FROM book_t', conn)
-    _df.to_sql(name='book', if_exists='replace', con=engine, index=True, index_label='isbn')
-    conn.close()
-
-    return _df
-
-
-@app.route('/', methods=['GET'])
+@app.route('/save', methods=['GET'])
 def scribble_book_saver():
     pubs = prod_pubs if RUN_MODE == 'prod' else test_pubs
 
@@ -128,8 +46,9 @@ def scribble_book_saver():
     print('Save dataframe in csv ...')
     total_df = pd.concat(df_list)
     csv_path = os.path.join(CSV_PATH, 'book_db.csv')
-    if os.path.exists(csv_path) is False:
-        total_df.to_csv(csv_path, encoding='utf-8')
+    if os.path.exists(csv_path):
+        csv_path = os.path.join(CSV_PATH, 'book_db_temp.csv')
+    total_df.to_csv(csv_path, encoding='utf-8')
 
     print('book_db.csv file successfully created at {}'.format(csv_path))
     return make_response(json.dumps(response, ensure_ascii=False).encode('utf-8'))
---
+++ book_data_saver.py
@@ -0,0 +1,134 @@
+import re
+import json
+import pandas as pd
+from urllib import request, parse
+from urllib.error import HTTPError
+
+from sqlalchemy import create_engine
+
+from config import MY_SQL_DATABASE_URI
+
+
+class NaverSearch:
+    offset = 1
+    default_display = 100
+
+    def __init__(self, display=None, client_id=None, client_secret=None, url=None, key=None):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.url = url + key + "="
+
+        if display is None:
+            self.display = self.default_display
+
+    def __call__(self, param, offset=None):
+        data = self.get_query_response(param)
+        if offset is None:
+            self.offset += self.display
+
+        return {
+            "offset": self.offset,
+            "data": data
+        }
+
+    def get_query_response(self, param):
+        param = parse.quote(param)
+        url = self.url + param + "&display=" + str(self.display)
+        req = request.Request(url=url)
+        req.add_header("X-Naver-Client-Id", self.client_id)
+        req.add_header("X-Naver-Client-Secret", self.client_secret)
+
+        try:
+            response = request.urlopen(req)
+        except HTTPError as e:
+            raise Exception('ERROR {}: {}'.format(e.code, e.reason))
+        else:
+            res_code = response.getcode()
+
+        res_body = response.read().decode('utf-8')
+        data = json.loads(res_body)
+
+        return data
+
+
+df_column = [
+    "title",
+    "author",
+    "publisher",
+    "isbn",
+    "description",
+    "image",
+    "link",
+    # "review",   # 출판사 서평
+    # "contents"  # 목차
+]
+
+
+prod_pubs = [
+    "위즈덤하우스",
+    "미디어그룹",
+    "시공사",
+    "문학동네",
+    "북이십일",
+    "김영사",
+    "창비",
+    "웅진싱크빅",
+    "도서출판길벗",
+    "민음사",
+    "알에이치코리아",
+    "다산북스",
+    "학지사",
+    "마더텅",
+    "아가페출판사",
+    "비룡소",
+    "한빛미디어",
+    "넥서스",
+    "박영사",
+    "쌤앤파커스",
+    "영진닷컴",
+    "가나문화콘텐츠",
+    "계림북스",
+    "을유문화사",
+    "자음과모음",
+    "개암나무"
+]
+
+test_pubs = prod_pubs[:3]
+
+
+def preprocessor(df):
+    reg_ex = '<.+?>'
+
+    df.loc[:, 'title'] = df['title'].map(lambda x: re.sub(reg_ex, '', x))
+    df.loc[:, 'author'] = df['author'].map(lambda x: re.sub(reg_ex, '', x))
+    df.loc[:, 'publisher'] = df['publisher'].map(lambda x: re.sub(reg_ex, '', x))
+    df.loc[:, 'description'] = df['description'].map(lambda x: re.sub(reg_ex, '', x))
+
+    df.loc[:, 'image'] = df['image'].map(lambda x: re.sub(reg_ex, '', x).rsplit('?', 1)[0])
+    df.loc[:, 'isbn'] = df['isbn'].map(lambda x: re.sub(reg_ex, '', x).rsplit(' ', 1)[1])
+
+    return df
+
+
+def dict_to_dataframe(items):
+    books = []
+    for item in items:
+        if item['isbn'] == '':
+            continue
+        books.append([item[col] for col in df_column])
+
+    df = pd.DataFrame(books, columns=df_column)
+    df.dropna(axis=0, how='any', inplace=True)
+
+    return preprocessor(df)
+
+
+def save_to_db(df):
+    engine = create_engine(MY_SQL_DATABASE_URI, encoding='utf-8')
+    conn = engine.connect()
+    df.to_sql('book_t', conn)
+    _df = pd.read_sql('SELECT * FROM book_t', conn)
+    _df.to_sql(name='book', if_exists='replace', con=engine, index=True, index_label='isbn')
+    conn.close()
+
+    return _df
