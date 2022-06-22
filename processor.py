a958bac31ff1e5e9340e859c04cb7b24b7131b91
soheeeep <tpfj@naver.com>
2022-06-22 21:20:23 +0900

    implement - tfidfvectorizer

--- processor.py
+++ processor.py
@@ -1,6 +1,14 @@
 import re
-import pandas as pd
+import os
 
+import numpy as np
+import pandas as pd
+from functools import lru_cache
+
+from config import CSV_PATH
+from nltk.corpus import stopwords
+from konlpy.tag import Okt
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 df_column = [
     "title",
@@ -38,3 +46,55 @@ def dict_to_dataframe(items):
     df.dropna(axis=0, how='any', inplace=True)
 
     return preprocessor(df)
+
+
+class BookPredictionProcessor:
+    okt = Okt()
+
+    def _tfidvectorizer(self, max_dfv, max_featuresv, min_dfv):
+        eng_stopwords = list(set(stopwords.words('english')))
+        book_stopwords = ['정가', '단가', '저자', '출판사', '소설가', '베스트셀러']
+        kor_stopwords_lines = open(os.path.join(CSV_PATH, 'stop_words.txt'), encoding='utf-8').readlines()
+        kor_stopwords = [line.rstrip() for line in kor_stopwords_lines]
+
+        stopwords_set = kor_stopwords + eng_stopwords + book_stopwords
+
+        tfidf_vectorizer = TfidfVectorizer(
+            max_df=max_dfv,
+            max_features=max_featuresv,
+            min_df=min_dfv,
+            stop_words=stopwords_set,
+            use_idf=True,
+            ngram_range=(1, 1),
+            dtype=np.int64
+        )
+        return tfidf_vectorizer
+
+    @lru_cache(maxsize=2)
+    def tfidvectorizer(self, max_dfv, max_featuresv, min_dfv):
+        return self._tfidvectorizer(max_dfv, max_featuresv, min_dfv)
+
+    def contents_vectorizer(self, data, columns, max_dfv=0.8, max_featuresv=1000, min_dfv=0.02):
+        """
+        도서 데이터에 대한 벡터화를 수행하는 경우, columns에 ['description', 'pub_review', 'detail']를 전달한다.
+        도서 카테고리에 대한 벡터화를 수행하는 경우, columns에 ['category_d1', 'category_d2', 'category_d3']를 전달한다.
+        """
+        tfidf_vectorizer = self.tfidvectorizer(max_dfv=max_dfv, max_featuresv=max_featuresv, min_dfv=min_dfv)
+        corpus = []
+        for record in data:
+            docs = ''
+            for c in columns:
+                if c in record and record[c] and record[c] != '':
+                    docs += " " + str(record[c])
+                corpus.append(' '.join(self.okt.nouns(docs)))
+
+        tfidf_matrix = tfidf_vectorizer.fit_transform(corpus)
+        tfidf_vocs = tfidf_vectorizer.vocabulary_
+        tfidf_arr = tfidf_matrix.toarray()
+
+        count = tfidf_arr.sum(axis=0)
+        idx = np.argsort(-count)        # 내림차순 정렬 index
+        feature_name = np.array(tfidf_vectorizer.get_feature_names_out())
+        words = list(zip(feature_name[idx], count[idx]))
+
+        return tfidf_vocs, tfidf_arr, tfidf_matrix, words
